f4b05b287e3f36202d571f7f2fde2a8cd702a9c6
Zaur Giyasov <zagiyas@mera.ru>
2018-06-06 13:40:06 +0300

    Initial Commit

---
+++ BlockDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BlockDemo
+//
+//  Created by Zaur Giyasov on 06/06/2018.
+//  Copyright © 2018 Zaur Giyasov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
