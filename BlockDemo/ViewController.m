b11862ef273e50cfbf94c0170d116d35010aecc9
Zaur Giyasov <zagiyas@mera.ru>
2018-06-07 18:09:47 +0300

    // C lang - begin

--- BlockDemo/ViewController.m
+++ BlockDemo/ViewController.m
@@ -22,6 +22,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    // void with array game
+    //[self arrayGames];
+    
+    // void C Lang
+    [self cLanguageFirst];
+    
+    /*
+    
     // void with different blocks
     [self differentExamples];
     // Outputs:
@@ -45,8 +53,133 @@
     //BlockDemo[10774:964788] Run other thread...
     //BlockDemo[10774:964788] thread work...
     //Example: value = 0, after work value = 49500000
+     
+     */
     
 }
+
+-(void)cLanguageFirst {
+    /// printf tab & fix
+    printf("|555|\n|\t\t\t 555|\n|555\t\t\t|\n");
+    printf("\n");
+    printf("Words \t\t\t 59\n");
+    printf("Words %13d\n", 59);
+    
+    printf("Letters \t\t %d\n", 1004);
+    printf("Digits \t\t\t %d\n\n", 8);
+    
+    //по правому краю (Для выравнивания по левому краю перед числом ставится знак минус) (не получается)
+    printf("Words %13-d\n", 59);
+    
+    printf("Letters \t\t %-d\n", 1004);
+    printf("Digits \t\t\t %-d", 8);
+    
+    /// size
+    printf("\n\n /// size\n");
+    int i = 8;
+    long long l = 8;
+    short s = 8;
+    char c = '8';
+    bool b = true;
+    bool b2 = false;
+    printf("size i = %d\nsize l = %d\nsize s = %d\nsize c = %d\nsize b = %d\nsize b2 = %d", sizeof(i), sizeof(l), sizeof(s), sizeof(c), sizeof(b), sizeof(b2));
+    
+    /// chars
+    printf("\n\n/// chars \n");
+    char ch = 63;
+    unsigned char uch = 'r';
+    short j = 'b', k = 99;
+    printf("%c == %d\n", ch, ch);
+    printf("%c == %d\n", uch, uch);
+    printf("\"short j = 'b', k = 99\": %c, %c", j, k);
+    
+    /// doobles
+    printf("\n\n/// doobles \n");
+    double a = 0.0005;
+    printf("%f\n", a);
+    printf("%g\n", 0.0005);
+    printf("%g\n", 0.00005);
+    printf("%e\n", 0.0005);
+    
+    printf("%.2f\n", 0.2398);
+    printf("%10.2f", 0.2398);
+    
+    /// arrays
+    printf("\n\n/// arrays \n");
+    //#define N 10
+    int N = 10;
+    int arr[5], nums[N];
+    float f_arr[100];
+    char str[80];
+    
+    char vol[] = {'a', 'e', 'i', 'o', 'u', 'y'};
+    float f_arr2[6];
+    
+    f_arr2[0] = 25.3;
+    f_arr2[4] = 34.2;
+    printf("\"vol[4], f_arr2[0]\": %c, %.2f\n", vol[4], f_arr2[0]);
+    
+    /// strings
+     printf("\n\n/// strings \n");
+    char string1[] = {'z', 'a', 'u', 'r', 'n', 'n', '\0'};
+    // is equal (string1[] == string2[])
+    char string2[] = "zaurnn";
+    
+    printf("%s = size = %d\n", string1, sizeof(string1));
+    printf("%s = size = %d\n", string2, sizeof(string2));
+    
+}
+
+-(void)arrayGames {
+
+    int numberOfElements = 10;
+    int array[numberOfElements];
+    char *str = "Hello world!";
+    
+    array[0] = 87;
+    array[1] = 8;
+    array[2] = 1;
+    array[3] = 153;
+    array[4] = 3;
+    array[5] = 613;
+    array[6] = 4;
+    array[7] = 44;
+    array[8] = 4;
+    array[9] = 4;
+    
+    for (int i = 0; i < numberOfElements; i++) {
+        printf("Array[numberOfElements] : element %d = %d\n", i, array[i]);
+    }
+    
+    int temp[1];
+    int sortTemp[numberOfElements];
+    for (int i = 0; i < numberOfElements; i++) {
+        
+        
+    }
+    
+    //    array[0] = 'H';
+    //    array[1] = 'e';
+    //    array[2] = 'l';
+    //    array[3] = 'l';
+    //    array[4] = 'o';
+    //    array[5] = 'w';
+    //    array[6] = 'o';
+    //    array[7] = 'r';
+    //    array[8] = 'l';
+    //    array[9] = '\0';
+    
+    
+
+    
+    for (int i = 0; i < 13; i++) {
+        printf("element %d = %c\n", i, str[i]);
+    }
+    
+    printf("%s\n", str);
+    
+}
+
 
 
 // A block declaration follows the next syntax pattern:
